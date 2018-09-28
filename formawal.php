d39e854dc542b5e6907f7ac259f4acdbcafd46f2
Muhammad Reza Maulana <43521076+MuhammadRezaMaulana@users.noreply.github.com>
2018-09-28 09:52:51 +0700

    formawal.php
    Form Login

--- formawal.php
+++ formawal.php
@@ -1,5 +1,19 @@
-<?php
+<?php 
+	if (isset($_POST['submit'])) {
 
+		include "hobi.php";
+		
+	}
+	
+	<form action="<?php echo $_SERVER['PHP_SELF'];?>" method ="POST">
+		
+		<label for="username">Username : </label>
+		<input type="text" name="username"><br><br>
+		
+		<label for="password">Password : </label>
+		<input type="password" name="password"><br><br>
 
+		<input type="submit" name="submit" value="LOGIN">
+	</form>
 
 ?>
